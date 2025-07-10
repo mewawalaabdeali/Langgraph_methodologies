1b54f9a8979cbcaf47845b8c2c5f1c2d700b3a99
ali <ali_abde37@outlook.com>
2025-07-10 10:35:37 -0400

    LangsmitH

---
+++ Debugging/openai_agent.py
@@ -0,0 +1,63 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langchain_openai import ChatOpenAI
+from langgraph.graph import END, START
+from langgraph.graph.state import StateGraph
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
+from langchain_core.tools import tool
+from langchain_core.messages import BaseMessage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class State(TypedDict):
+    messages:Annotated[list[BaseMessage], add_messages]
+
+model = ChatOpenAI(temperature=0)
+
+def make_default_graph():
+    graph_workflow = StateGraph(State)
+
+    def call_model(state):
+        return {"messages":[model.invoke(state['messages'])]}
+    
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_edge("agent", END)
+    graph_workflow.add_edge(START, "agent")
+
+    agent=graph_workflow.compile()
+    return agent
+
+def tool_graph():
+    """Make a tool-calling agent"""
+    @tool
+    def add(a:float, b:float):
+        """Add two numers."""
+        return a+b
+    
+    tool_node = ToolNode([add])
+    model_with_tool = model.bind_tools([add])
+    def call_model(state):
+        return {"messages":[model_with_tool.invoke(state["messages"])]}
+    
+    def should_continue(state: State):
+        if state["messages"][-1].tool_calls:
+            return "tools"
+        else:
+            return END
+        
+    graph_workflow = StateGraph(State)
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_node("tools", tool_node)
+    graph_workflow.add_edge("tools", "agent")
+    graph_workflow.add_edge(START, "agent")
+    graph_workflow.add_conditional_edges("agent", should_continue)
+    agent = graph_workflow.compile()
+    return agent
+
+
+
+agent=tool_graph()
+
